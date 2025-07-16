51efc0c3bbeceb8ce2e439b4f3c75aec80cc10bb
thaildhe172591 <121850511+thaildhe172591@users.noreply.github.com>
2025-07-16 21:54:06 +0700

    ud

--- app/src/main/java/com/example/netflex/network/ApiService.java
+++ app/src/main/java/com/example/netflex/network/ApiService.java
@@ -6,16 +6,23 @@ import com.example.netflex.model.Serie;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 import com.example.netflex.model.PaginatedResponse;
 import com.example.netflex.model.Genre;
 import com.example.netflex.model.Country;
+import com.example.netflex.model.SignInRequest;
+import com.example.netflex.model.SignInResponse;
 
 import retrofit2.http.Query;
 
 public interface ApiService {
+
+    @POST("auth/signin")
+    Call<SignInResponse> signIn(@Body SignInRequest request);
 
     @GET("genres")
     Call<PaginatedResponse<Genre>> getGenres(
